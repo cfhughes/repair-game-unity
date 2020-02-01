92d334f51074232d83f2c1991f701a9b0c907c23
ParisiDesign <ben@parisidesign.com>
2020-02-01 12:51:56 -0800

    first pass at door

---
+++ Repair/Assets/Door.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isClosed = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(isClosed);
+    }
+
+    public void Toggle()
+    {
+        isClosed = !isClosed;
+        gameObject.SetActive(isClosed);
+    }
+}
