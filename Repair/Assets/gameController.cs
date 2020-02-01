befe6b01e513a45269b86f7d97406da5ad552142
ParisiDesign <ben@parisidesign.com>
2020-02-01 09:17:50 -0800

    initial project commit VR with 2 camera system

---
+++ Repair/Assets/gameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+
+    public GameObject testObj;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            //print("space key was pressed");
+            testObj.SetActive(!testObj.activeInHierarchy);
+        }
+    }
+}
