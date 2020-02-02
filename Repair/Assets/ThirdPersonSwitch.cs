fb8037d26b28ec956ca94daa48f2e7ac08e0fd29
ParisiDesign <ben@parisidesign.com>
2020-02-01 18:09:11 -0800

    switch for pc player to reset pressure guages

---
+++ Repair/Assets/ThirdPersonSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonSwitch : MonoBehaviour
+{
+    public List<PressureGauge> pressureGauges;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        print("trigger enter!");
+        if(c.gameObject.tag == "Player")
+        {
+            print("player enter!");
+            foreach(PressureGauge pressureGuage in pressureGauges)
+            {
+                pressureGuage.Reset();
+            }
+        }
+    }
+}
