e65ca0b5380733dd07b7a67bf264785d1d99feff
ParisiDesign <ben@parisidesign.com>
2020-02-01 20:33:52 -0800

    no message

--- Repair/Assets/PressureGauge.cs
+++ Repair/Assets/PressureGauge.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class PressureGauge : MonoBehaviour
 {
     public float overloadTotalTime;
+    public ParticleSystem smoke;
 
     float overloadCurrentTime = 0;
-    Animator animator;
+    float minAngle = 0;
+    float maxAngle = 60;
+    float minEmission = 8;
+    float maxEmission = 16;
 
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
 
     void Update()
     {
         overloadCurrentTime += Time.deltaTime;
         float overloadPercent = Mathf.Min(overloadCurrentTime/overloadTotalTime, 1);
-        animator.Play("PressureGaugeAnim", 0, overloadPercent);
+        var sh = smoke.shape;
+        sh.angle = Mathf.Lerp(minAngle, maxAngle, overloadPercent);
+        var em = smoke.emission;
+        if(overloadPercent < .3)
+        {
+            em.rateOverTime = 0;
+        }
+        else
+        {
+            em.rateOverTime = Mathf.Lerp(minEmission, maxEmission, overloadPercent);
+        }
     }
 
     public void Reset()
